425b819aa798bc8dd139df3f749452d3b1a2bfa8
luke123adams <lxa207@gmail.com>
2022-11-04 14:25:42 +0000

    E2E testing completed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,17 @@ describe('toDo list site', () => {
     cy.get('#input-item').type('buy a laptop')
     cy.get('#complete-date').type('2012-11-20')
     cy.get('#button').click()
+    cy.get('ul').contains('buy a laptop')
   })
+   it('delete an item', () => {
+    cy.get('#todos > li:nth-child(1) > button').click()
+    cy.get('ul').should('not.have.text', 'Walk the dog')
+   })
+   it('refresh the page', () => {
+    cy.reload()
+    cy.get('ul').contains('Feed the computer')
+    cy.get('ul').contains('buy a laptop')
 
+   })
 })
-// A user visits the site(done), adds a new todo to the list, deletes a todo from the list, refreshes the page and the todos are still persisted/saved. something 
+// A user visits the site(done), adds a new todo to the list(DONE), deletes a todo from the list (DONE), refreshes the page and the todos are still persisted/saved. something 
